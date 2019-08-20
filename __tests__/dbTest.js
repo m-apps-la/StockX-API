027f95426c987c82f9988f86edefd8a1a395af38
Mike Mulderink <mulderink1@gmail.com>
2019-08-20 14:08:20 -0700

    Added comment to test file

--- __tests__/dbTest.js
+++ __tests__/dbTest.js
@@ -3,7 +3,6 @@
 const request = require('supertest'),
   app = 'localhost:8080'
 
-
 describe('Test the root path', () => {
   test('It should response the GET method', () => {
     return request(app).get('/').expect('Catch All');
